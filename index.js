21b959409f262407bf44f2d55264ad13ee1dbe26
Bhakti Shinde <shindebhakti233@gmail.com>
2025-07-05 14:42:03 +0530

    index.jsx

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+// Import React core library
 import React from "react";
+
+// Import the method to render React app to the DOM
 import ReactDOM from "react-dom/client";
+
+// Import the main App component
 import App from "./App";
 
+// Create a root element where the React app will be mounted
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the <App /> component inside the root element
 root.render(<App />);
